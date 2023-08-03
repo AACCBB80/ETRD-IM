d499235603f185619fa594590f6f8e107e8fe6c7
AACCBB80 <89084279+AACCBB80@users.noreply.github.com>
2023-08-03 12:53:36 -0500

    Update ETRD v2 Beta.lua

--- ETRD v2 Beta.lua
+++ ETRD v2 Beta.lua
@@ -80,6 +80,8 @@ local version = 2
 local currentY = 10
 local room = DefaultRoom
 MANAGER.print("ETRD (PowderIM) v".. version .." loaded!")
+MANAGER.print("ETRD (PowderIM) by aaccbb.")
+windowopen = 0
 
 --======================--
 
@@ -90,19 +92,9 @@ if (AutoUpdate == "true") then
 	while versionFetch:status() == "running" do
 	end
 	versionFetchOut, tmp = versionFetch:finish()
-	
-	updateCheck = http.get("https://raw.githubusercontent.com/AACCBB80/ETRD-IM/main/UPD.ATE")
-	while updateCheck:status() == "running" do
-	end
-	updateCheckOut, tmp = updateCheck:finish()
-	
-	if (updateCheck ~= "no\n") then
-		if (versionFetchOut ~= "2\n") then
-			print("[ETRD v".. version .."] Updating to v".. versionFetchOut)
-			tpt.getscript(245, "./scripts/downloaded/245 aaccbb-ETRD _Formerly PowderIM_.lua", 1, 0)
-		else
-			print("[ETRD v".. version .."] No updates found!")
-		end
+	if (versionFetchOut ~= "2\n") then
+		print("[ETRD v".. version .."] Updating to v".. versionFetchOut)
+		tpt.getscript(245, "./scripts/downloaded/245 aaccbb-ETRD _Formerly PowderIM_.lua", 1, 0)
 	else
 		print("[ETRD v".. version .."] No updates found!")
 	end
@@ -269,7 +261,7 @@ cfgY = 20
 
 local cfg1L = Label:new(48, (cfgY - 12), 0, 16, "Default Room")
 
-local cfg1 = Textbox:new(10, cfgY, 45, 16, DefaultRoom, "Default Room")
+local cfg1 = Textbox:new(10, cfgY, 80, 16, DefaultRoom, "Default Room")
 
 cfgY = cfgY + 30
 
@@ -355,6 +347,7 @@ cfgClose:action(
 		MANAGER.savesetting("aaccbbETRDIM","dopings",doPings)
 		MANAGER.savesetting("aaccbbETRDIM","autoupdate",AutoUpdate)
 		interface.closeWindow(cfgWindow)
+		windowopen = 0
 	end
 )
 
@@ -461,9 +454,13 @@ else
 
 --======================--
 
-closeButton:action(function() interface.closeWindow(chatwindow) end)
+closeButton:action(function() interface.closeWindow(chatwindow)
+windowopen = 0
+ end)
 
-chatwindow:onTryExit(function() interface.closeWindow(chatwindow) end) -- Allow the default exit events
+chatwindow:onTryExit(function() interface.closeWindow(chatwindow)
+windowopen = 0
+ end) -- Allow the default exit events
 
 --cfgClose:action(function() interface.closeWindow(cfgWindow) end)
 
@@ -509,9 +506,48 @@ cfgWindow:addComponent(cfg8L)
 
 --======================--
 
-	local function keypressed(key, keynum, mod, evt)
+powderim:action(function(sender)
+--getMessagesCo = coroutine.create(getMessages)
+--coroutine.resume(getMessagesCo)
 
+----[[
+	windowopen = 1
+	interface.showWindow(chatwindow)
+	fetch = http.get(ChatServer.. room ..".txt")
+			while fetch:status() == "running" do
+			end
+			chat, tmp = fetch:finish()
+			if (tmp ~= 200) then
+				print("Server error!\nNotify aaccbb.")
+			else
+				chat = string.gsub(chat, "\x0A", "")
+				--chat = string.gsub(chat, "○", "\n")
+				chatu = ""
+				local pingname = "@".. tpt.get_name() .." "
+				local pingname2 = " @".. tpt.get_name()
+				for word1 in string.gmatch(chat, '([^○]+)') do
+					if (doPings == "true") then
+						if string.match(word1, pingname) then
+							chatu = chatu .."\x0F\xED\xE6\x61".. word1 .."\n"
+						elseif string.match(word1, pingname2) then
+							chatu = chatu .."\x0F\xED\xE6\x61".. word1 .."\n"
+						else
+							chatu = chatu .."\x0F\xFF\xFF\xFF".. word1 .."\n"
+						end
+					else
+						chatu = chatu .. word1 .."\n"
+					end	
+				end
+				--interface.showWindow(chatwindow)
+				chat1:text(chatu)
+			end
+			--]]--
+		end)
+
+
+	local function keypressed(key, keynum, mod, evt)
 		if evt == 2 and key == "/" then
+			windowopen = 1
 			interface.showWindow(chatwindow)
 			fetch = http.get(ChatServer.. room ..".txt")
 			while fetch:status() == "running" do
@@ -578,46 +614,53 @@ cfgWindow:addComponent(cfg8L)
 		end	
 		--end
 		--end
-	end
-	
-	
-powderim:action(function(sender)
---getMessagesCo = coroutine.create(getMessages)
---coroutine.resume(getMessagesCo)
-
-----[[
-	interface.showWindow(chatwindow)
-	fetch = http.get(ChatServer.. room ..".txt")
+		
+		if evt == 2 and keynum == 13 and windowopen == 1 then
+			msg = testTextbox:text()
+			fetch = http.post(ChatServer .."chat.php", {
+			["user"] = tpt.get_name(),
+			["msg"] = msg,
+			["room"] = room
+			})
 			while fetch:status() == "running" do
 			end
 			chat, tmp = fetch:finish()
 			if (tmp ~= 200) then
-				print("Server error!\nNotify aaccbb.")
+				chat1:text("Server error!\nNotify aaccbb.")
 			else
-				chat = string.gsub(chat, "\x0A", "")
-				--chat = string.gsub(chat, "○", "\n")
-				chatu = ""
-				local pingname = "@".. tpt.get_name() .." "
-				local pingname2 = " @".. tpt.get_name()
-				for word1 in string.gmatch(chat, '([^○]+)') do
+				fetch = http.get(ChatServer.. room ..".txt")
+				--interface.showWindow(loadingwindow)
+				while fetch:status() == "running" do
+				end
+				--interface.closeWindow(loadingwindow)
+				chat, tmp = fetch:finish()
+				if (tmp ~= 200) then
+					chat1:text("Server error!\nNotify aaccbb.")
+				else
+					chat = string.gsub(chat, "\x0A", "")
+					--chat = string.gsub(chat, "○", "\n")
+					chatu = ""
+					local pingname = "@".. tpt.get_name() .." "
+					local pingname2 = " @".. tpt.get_name()
+					for word1 in string.gmatch(chat, '([^○]+)') do
 					if (doPings == "true") then
 						if string.match(word1, pingname) then
 							chatu = chatu .."\x0F\xED\xE6\x61".. word1 .."\n"
 						elseif string.match(word1, pingname2) then
 							chatu = chatu .."\x0F\xED\xE6\x61".. word1 .."\n"
-						else
+							else
 							chatu = chatu .."\x0F\xFF\xFF\xFF".. word1 .."\n"
 						end
-					else
-						chatu = chatu .. word1 .."\n"
-					end	
-				end
-				--interface.showWindow(chatwindow)
-				chat1:text(chatu)
-			end
-			--]]--
-		end)
-
+		else
+			chatu = chatu .. word1 .."\n"
+		end	
+	end
+	chat1:text(chatu)
+	end
+	end
+		end
+		
+	end
 
 	tpt.register_keypress(keypressed)
 	
@@ -631,7 +674,7 @@ powderim:action(function(sender)
 ║RebMiami, the one who made║▒
 ║ETRDv1 (PowderIM) work by ║▒
 ║adding 1 line and persuade║▒
-║me to be less lazy, enough║▒
+║me to be less lazy enough ║▒
 ║  to continue writing v2. ║▒
 ╠══════════════════════════╣▒
 ║ Maticzpl, he helped with ║▒
@@ -648,7 +691,7 @@ powderim:action(function(sender)
 ╠══════════════════════════╣▒
 ║If you think your name is ║▒
 ║missing, DM me at aaccbb80║▒
-║                  (#9095) ║▒
+║                 (Discord)║▒
 ╚══════════════════════════╝▒
 ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
 --]]
